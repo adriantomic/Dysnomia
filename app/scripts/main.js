80fb4835deae2d93d110f7fc5cee56ab7e98a4e5
Adrian Tomic <adrian.tomic@doberman.se>
2016-05-25 17:54:14 +0200

    testing stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+
+var cardComponent = Vue.extend({
+	template: '<li>A custom component!</li>'
+})
+
+Vue.component('card', cardComponent)
+
+// create a root instance
+new Vue({
+  el: '#app',
+  data: {
+    debug: true,
+    people: []
+  },
+  ready: function(){
+    this.loadPeople();
+  },
+  methods: {
+    loadPeople: function() {
+    	this.$http.get('data/people.json'.then(function (response) {
+        this.$set('people', response.data);
+      });
+    }
+  }
+});
